f44531fac59f22f676a29b3e34fb9b9b275319ca
Freddi Gyara <freddi@o3bank.com>
2019-05-08 15:43:06 +0100

    Some more regexs

--- _experiments/script/convert.bat
+++ _experiments/script/convert.bat
@@ -4,12 +4,12 @@ rem rm -rf out
 
 xcopy .\in\*.html .\out\*.md /i /s /q
 
-grep -rl h1 ./out/*.md | xargs sed -i "s/<h1[^>]*>/\n## /g"
-grep -rl h2 ./out/*.md | xargs sed -i "s/<h2[^>]*>/\n### /g"
-grep -rl h3 ./out/*.md | xargs sed -i "s/<h3[^>]*>/\n#### /g"
-grep -rl h4 ./out/*.md | xargs sed -i "s/<h4[^>]*>/\n##### /g"
-grep -rl h5 ./out/*.md | xargs sed -i "s/<h5[^>]*>/\n###### /g"
-grep -rl h6 ./out/*.md | xargs sed -i "s/<h6[^>]*>/\n####### /g"
+grep -rl h1 ./out/*.md | xargs sed -i -E "s/<h1[^>]*>/\n## /g"
+grep -rl h2 ./out/*.md | xargs sed -i -E  "s/<h2[^>]*>/\n### /g"
+grep -rl h3 ./out/*.md | xargs sed -i -E  "s/<h3[^>]*>/\n#### /g"
+grep -rl h4 ./out/*.md | xargs sed -i -E  "s/<h4[^>]*>/\n##### /g"
+grep -rl h5 ./out/*.md | xargs sed -i -E  "s/<h5[^>]*>/\n###### /g"
+grep -rl h6 ./out/*.md | xargs sed -i -E  "s/<h6[^>]*>/\n####### /g"
 
 grep -rl h1 ./out/*.md | xargs sed -i "s/<\/h1>/\n/g"
 grep -rl h2 ./out/*.md | xargs sed -i "s/<\/h2>/\n/g"
@@ -26,8 +26,8 @@ grep -rl br ./out/*.md | xargs sed -i "s/<br \/>/\n/g"
 grep -rl strong ./out/*.md | xargs sed -i "s/<strong>/ **/g"
 grep -rl strong ./out/*.md | xargs sed -i "s/<\/strong>/** /g"
 
-grep -rl em ./out/*.md | xargs sed -i "s/<em>/ **/g"
-grep -rl em ./out/*.md | xargs sed -i "s/<\/em>/** /g"
+grep -rl em ./out/*.md | xargs sed -i -E "s/<em[^>]*>/ _/g"
+grep -rl em ./out/*.md | xargs sed -i "s/<\/em>/_ /g"
 
 grep -rl body ./out/*.md | xargs sed -i -E "s/<body[^>]*>//g"
 grep -rl body ./out/*.md | xargs sed -i -E "s/<\/body>/\n/g"
@@ -80,5 +80,8 @@ grep -rl html ./out/*.md | xargs sed -i "s/<\/html>//g"
 grep -rl lt ./out/*.md | xargs sed -i "s/&lt;/</g"
 grep -rl gt ./out/*.md | xargs sed -i "s/&gt;/>/g"
 grep -rl quot ./out/*.md | xargs sed -i "s/&quot;/\"/g"
+
+grep -rl Notes ./out/*.md | xargs sed -i "s/**Notes**/Notes/g"
+
 
 cd script
