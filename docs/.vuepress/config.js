9c31c86b8ca0f9c895831ff0cbbe110422907ead
Vitalijs Lobins <vic@93digital.co.uk>
2022-09-22 12:26:04 +0100

    changed favicons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,11 @@ module.exports = {
     base: '/read-write-api-site3/',
     title: '',
     description: 'OBIE Read/Write API Standards',
-    head,
+    head: [
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/img/icons/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/img/icons/favicon-16x16.png"}],
+        ['link', { rel: "shortcut icon", href: "/assets/img/icons/favicon.ico"}],
+    ],
     themeConfig: {
         sidebar: sidebar,
         nav,
