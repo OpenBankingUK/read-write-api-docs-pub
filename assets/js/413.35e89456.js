d1ed0dcb83ff20b6d5a457959681a0cc82ac835e
Wojciech Kropidlowski <w.kropidlowski@accenture.com>
2022-09-29 11:46:53 +0200

    manual deploy refresh sep 2022

--- assets/js/413.35e89456.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[413],{1461:function(t,e,s){"use strict";s.r(e);var n=s(6),l=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("Files Realted to offers spec")])])}),[],!1,null,null,null);e.default=l.exports}}]);
